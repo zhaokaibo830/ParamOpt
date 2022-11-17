32d3a7af0e13f50bc1747ca4c351ec4b9fa370a8
zkb <18829036561@163.com>
2022-11-17 16:16:59 +0800

    commit

---
+++ gentask/gentask_same_spark_param.py
@@ -0,0 +1,112 @@
+import os
+import json
+import random
+import pandas as pd
+import numpy as np
+from urllib import request, parse
+import time
+
+def get_performance(params):
+    """
+    接系统获取性能值
+    :param params: {'param1': 10, 'param2': 's1', ...}
+    :return: 性能值
+    """
+
+    headers = {"user-agent": "Mizilla/5.0"}
+    value = parse.urlencode(params)
+    # print(value)
+    # data_ = value.encode('utf-8')
+    # print(data_)
+    # return -1
+
+    # 请求url
+    # http://47.104.101.50:8080/experiment/redis?     redis
+    # http://47.104.101.50:9090/experiment/redis?     cassandra
+    # http://47.104.101.50:8088/x264/exec?  x264
+    # http://47.104.101.50:8081/jmeter?  tomcat
+    req1 = request.Request('http://192.168.0.168:9002/api/spark/runWordcountForTime?%s' % value, headers=headers)  # 这样就能把参数带过去了
+    print('http://192.168.0.168:9002/api/spark/runWordcountForTime?%s' % value)
+    # 下面是获得响应
+    i = 0
+    # url_ = 'http://47.104.101.50:8081/jmeter'
+    while True:
+        try:
+            # print(data_)
+            f = request.urlopen(req1, timeout=360)
+
+            # print(f)
+            Data = f.read()
+            data = Data.decode('utf-8')
+            if data == "error":
+                print("查不到，报errror，重新尝试\n")
+                f.close()
+                if i < 3:
+                    i += 1
+                    time.sleep(3)
+                else:
+                    return -1.0
+            else:
+                print(data)
+                print("get_performance成功\n")
+                f.close()
+                break
+
+        except Exception as e:
+            print(e)
+            i = i + 1
+            if i < 3:
+                time.sleep(3)
+            else:
+                return -1.0
+    return (float)(data)
+
+# 随机采样
+def random_sample(selected_params, sample_num):
+    """
+    对不太重要的参数进行随机采样
+    :param selected_params: selected_params字典形式
+    :param sample_num: 采样数量
+    :return: 采样结果
+    """
+
+    # 随机采样开始
+    samples = []
+    for _ in range(sample_num):
+        samples.append([])
+    for key in selected_params.keys():
+        if selected_params[key][0] == 'int':
+            for i in range(sample_num):
+                samples[i].append(random.randint(selected_params[key][1][0] + 1, selected_params[key][1][1]))
+        elif selected_params[key][0] == 'float':
+            for i in range(sample_num):
+                samples[i].append(random.uniform(selected_params[key][1][0] + 0.1, selected_params[key][1][1]))
+        elif selected_params[key][0] == 'enum':
+            for i in range(sample_num):
+                samples[i].append(random.choice(selected_params[key][1]))
+        else:
+            continue
+
+    for i in range(sample_num):
+        samples[i].append(0)
+
+    df_samples = pd.DataFrame(samples, columns=list(selected_params.keys()) + ["PERF"])
+
+    for i in range(len(samples)):
+        top_one = dict(df_samples.iloc[i, :-1])
+        value=get_performance(top_one)
+        df_samples.iloc[i][-1]=value
+    temp_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'taskdata/{sourcetask_1}.csv')
+    df_samples.to_csv(temp_filename, index=False)
+
+if __name__=="__main__":
+    confs = json.load(open('paras.json'), strict=False)
+    selected_params = confs['common_params']['all_params']
+    if not os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)),'taskdata')):
+        os.makedirs(os.path.join(os.path.dirname(os.path.abspath(__file__)),'taskdata'))
+
+    random_sample(selected_params, 50)
+
+
+
+
